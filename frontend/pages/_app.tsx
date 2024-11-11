7ddf570c200e902f79e72f544440d941e537ec0b
saa20ce <saa20ce@gmail.com>
2024-11-11 23:18:37 +0700

    updated frontend pages

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { appWithTranslation } from 'next-i18next';
+import '../styles/global.css';
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default appWithTranslation(MyApp);
---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,20 @@
+// frontend/pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Добавьте метатеги и шрифты, если нужно */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
