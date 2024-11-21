3f8d752abc817ef9f1074e1c44a6f12c1a194daf
saa20ce <saa20ce@gmail.com>
2024-11-21 12:27:03 +0700

    updated homepage

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,23 @@
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import Footer from '../components/Footer';
-import Layout from '@/components/Layout';
+import Layout from '@/components/Layout/Layout';
+import Text from '@/components/Text/Text';
+import MainMenu from '@/components/MainMenu/MainMenu';
 
 export default function Home() {
+
   return (
     <Layout>
-      <Hero />
+      <div className="md:w-1/2 space-y-6">
+        <Text as='h1' className='text-4xl font-bold'>
+          Разработка и дизайн
+          </Text>
+        <Text as='p' className='text-lg'>
+          Современная веб-студия, занимающееся разработкой продающих сайтов и дизайнов социальных сетей
+        </Text>
+        <MainMenu />
+      </div>
+      <div className="md:w-1/2 mt-8 md:mt-0">
+        <img src="/images/Man.png" alt="3D Human" className="w-full h-auto"/>
+      </div>
     </Layout>
   );
 }
